db72a9649bfe76e733e59c486c6e8548e7a26c09
Abhishek Pal <a9315296730@gmail.com>
2025-12-30 15:26:55 +0530

    fixed build issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000kB to silence warning if splitting isn't enough
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', 'axios'],
+          antd: ['antd'],
+        },
+      },
+    },
+  },
   server: {
     port: 3000,
     proxy: {
